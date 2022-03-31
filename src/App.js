8c51138c84151ed1b7f2eaa564911c78891d2a51
ewitulsk <ewitulsk@calpoly.edu>
2022-03-30 20:42:51 -0700

    can add and view greetings

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { Contract, providers } from "ethers";
+import {contractAddr, contractABI} from "./contractInfo"
 
 function App() {
+
+  const [greeting, setGreeting] = useState();
+  const [signer, setSigner] = useState();
+
+  const provider = new providers.Web3Provider(window.ethereum, "maticmum");
+
+  const readContract = new Contract(contractAddr, contractABI, provider);
+
+  async function connectWallet(){
+    await provider.send("eth_requestAccounts", []);
+    const _signer = provider.getSigner();
+    setSigner(_signer);
+    console.log("Account: ", await _signer.getAddress());    
+  }
+
+  async function setGreetingInContract(string){
+    if(signer){
+      const writeContract = new Contract(contractAddr, contractABI, signer);
+      const tx = writeContract.setGreeting(string);
+      console.log(tx)
+    }
+    else{
+      alert("Please Connect MetaMask!!!")
+    }
+  }
+
+  function submitForm(e){
+    e.preventDefault();
+    const _greeting = e.target.greeting.value
+    setGreetingInContract(_greeting);
+  }
+
+  useEffect( () => {
+    async function getGreeting(){
+      const greeting = await readContract.greet();
+      console.log(greeting);
+      await setGreeting(greeting);
+    }
+
+      const _signer = provider.getSigner();
+      if(_signer._address){
+        console.log("setting signer")
+        setSigner(_signer);
+      }
+      else if(! _signer){
+        alert("Please install MetaMask!")
+      }
+      
+
+    getGreeting();
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div onSubmit={submitForm}>
+      <button onClick={connectWallet}>Connect Wallet</button>
+      <br/>
+      Current Greeting: {greeting}
+      <form>
+        <label>Enter Greeting: </label>
+        <input type="text" name="greeting"/>
+        <button type="submit">Submit</button>
+      </form>
+
     </div>
   );
 }
---
+++ src/contractInfo.js
@@ -0,0 +1,41 @@
+export const contractAddr = "0x8b1A36652F9D3Bc85Fc0beDd288C26bD7D96d6E4";
+
+export const contractABI = [
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_greeting",
+				"type": "string"
+			}
+		],
+		"name": "setGreeting",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_greeting",
+				"type": "string"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"inputs": [],
+		"name": "greet",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
